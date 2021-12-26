1808174ef873f72cf23240abacb9aab9c6ed64fe
Camilla Minutilli <camilla.minutilli@gmail.com>
2021-12-26 18:31:07 +0100

    Style: cleaning

--- src/components/tomboloneNumbersGenerator.js
+++
@@ -1,8 +0,0 @@
-import { h } from "preact";
-import { NEW_TOMBOLONE_NUMBER } from "../constants/actions";
-
-export default ({ dispatch, number }) => (<section className="card__tombolone_number">
-    <button onClick={() => { dispatch({ type: NEW_TOMBOLONE_NUMBER }) }}>New number</button>
-    <div id="tombolone_number" className="card__box">{number || ''}</div>
-</section>)
-
