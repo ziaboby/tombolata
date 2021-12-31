4bf525d641e749f03fb156dcacb8df95ad712432
Camilla Minutilli <camilla.minutilli@gmail.com>
2021-12-31 17:48:00 +0100

    Refactor: changed default selection dom item

--- src/components/view.js
+++ src/components/view.js
@@ -1,12 +1,24 @@
 import { h } from "preact";
 import { SELECT_NUMBER } from "../constants/actions";
 
-const NumberBox = ({ dispatch, number, isSelected }) => (<div
-    className={"card__number" + (isSelected ? " card__number--selected" : '')}
-    onClick={() => { dispatch({ type: SELECT_NUMBER, number }) }}>
+const NumberBox = ({ dispatch, number }) => (
+  <div
+    className="card__number"
+    onClick={() => {
+      dispatch({ type: SELECT_NUMBER, number });
+    }}
+  >
     {number}
-</div>)
+  </div>
+);
 
-const View = ({ numbers, dispatch, isSelected }) => numbers.map(num => <li className="card__box">{num ? <NumberBox dispatch={dispatch} number={num} isSelected={isSelected(num)} /> : null}</li>)
+const View = ({ numbers, dispatch, isSelected }) =>
+  numbers.map((num) => (
+    <li
+      className={"card__box" + (isSelected(num) ? " card__box--selected" : "")}
+    >
+      {num ? <NumberBox dispatch={dispatch} number={num} /> : null}
+    </li>
+  ));
 
-export default View
+export default View;
