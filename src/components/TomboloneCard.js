ff6c636b0430057b951304b53b7301185e47ae75
Camilla Minutilli <camilla.minutilli@gmail.com>
2022-12-01 13:12:27 +0100

    Fix: unsupported attribute

--- src/components/TomboloneCard.js
+++ src/components/TomboloneCard.js
@@ -3,11 +3,7 @@ import View from "./view";
 
 export default ({ dispatch, numbers, isSelected }) => (
   <div className="card__container card__container--tombolone" role="grid">
-    <div
-      className="card card--tombolone"
-      role="row"
-      aria-multiselectable="false"
-    >
+    <div className="card card--tombolone" role="row">
       <View numbers={numbers} dispatch={dispatch} isSelected={isSelected} />
     </div>
   </div>
