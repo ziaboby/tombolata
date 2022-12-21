4706bab2ed4c05a5bf2060dfcd46d2afe31bb8b4
Camilla Minutilli <camilla.minutilli@gmail.com>
2022-12-21 22:35:41 +0100

    Fix: a11y - added label

--- src/components/GoToNumberCTA.js
+++ src/components/GoToNumberCTA.js
@@ -3,17 +3,20 @@ const FIELD_NAME = "GoToNumberCTA";
 export const GoToNumberCTA = () => (
   <section className="card-cell__cta">
     <form role="search" onSubmit={onSubmitHandler}>
-      <input
-        id={FIELD_NAME}
-        name={FIELD_NAME}
-        type="number"
-        inputmode="numeric"
-        min="1"
-        max="90"
-        step="1"
-        placeholder="Enter 1-90"
-        list="defaultNumbers"
-      />
+      <label>
+        Enter a number
+        <input
+          id={FIELD_NAME}
+          name={FIELD_NAME}
+          type="number"
+          inputmode="numeric"
+          min="1"
+          max="90"
+          step="1"
+          placeholder="1 - 90"
+          list="defaultNumbers"
+        />
+      </label>
       <datalist id="defaultNumbers">
         {Array(90)
           .fill(1)
@@ -24,7 +27,7 @@ export const GoToNumberCTA = () => (
             </option>
           ))}
       </datalist>
-      <button type="submit">Go to number</button>
+      <button type="submit">Go to its cell</button>
     </form>
   </section>
 );
