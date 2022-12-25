ce3b3992b93a8f705ae1f706aefc12bdadd57273
Camilla Minutilli <camilla.minutilli@gmail.com>
2022-12-25 12:19:35 +0100

    Fix: temp for public

--- server/index.js
+++ server/index.js
@@ -6,6 +6,6 @@ app.use("/api", api);
 
 app.listen(process.env.PORT || 3000);
 
-if (process.env.NODE_ENV === "development") {
-  app.use(express.static("public"));
-}
+//if (process.env.NODE_ENV === "development") {
+app.use(express.static("public"));
+//}
