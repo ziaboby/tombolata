2a443d63403bab77716d6d4ebfb2a31c756bbe23
Camilla Minutilli <camilla.minutilli@gmail.com>
2022-12-25 11:21:32 +0100

    New version deployed on vercel

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const api = require("./api");
+
+app.use("/api", api);
+
+app.listen(process.env.PORT || 3000);
+
+if (process.env.NODE_ENV === "development") {
+  app.use(express.static("public"));
+}
