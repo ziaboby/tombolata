eda3f698fbbc3c31f9ee5cb445edb67bd7ab25e0
Camilla Minutilli <camilla.minutilli@gmail.com>
2022-12-25 07:21:37 +0100

    Chore: added build customization

--- server/index.js
+++ server/index.js
@@ -5,3 +5,7 @@ const api = require("./api");
 app.use("/api", api);
 
 app.listen(process.env.PORT || 3000);
+
+if (process.env.NODE_ENV === "development") {
+  app.use(express.static("public"));
+}
